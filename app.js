14c1ac9a7728d0205f9ceb8be079039b1f4a79db
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-09 11:00:03 +0200

    add invert logo et nav

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-const btnMenu = document.querySelector('.nav-hb');
-
-const navOver = document.querySelector('.nav-wrapper-over');
-
-console.log(btnMenu)
-console.log(navOver)
-btnMenu.addEventListener('click', () => {
-  navOver.classList.toggle('active')
-})
-
 // loader
 
-const loader = document.querySelector('.loader');
-const main = document.querySelector('.main');
+const loader = document.querySelector(".loader");
+const main = document.querySelector(".main");
 
 setTimeout(() => {
-  loader.classList.add('noOpacity');
+  loader.classList.add("noOpacity");
 }, 1000);
 
-setTimeout(() =>{
-  loader.classList.add('display-none');
+setTimeout(() => {
+  loader.classList.add("display-none");
 }, 2500);
 
 setTimeout(() => {
-  main.classList.remove('display-none');
-}, 2000)
+  main.classList.remove("display-none");
+}, 2000);
 
 setTimeout(() => {
-  main.classList.remove('noOpacity');
+  main.classList.remove("noOpacity");
 }, 2500);
 
+// TOGGLE NAVBAR
+document.querySelector(".nav-hb").addEventListener("click", () => {
+  const navOver = document.querySelector(".nav-wrapper-over");
+  const navLogo = document.querySelector(".nav-logo");
+  const navHb = document.querySelector(".nav-hb");
+  navOver.classList.toggle("active");
+  navLogo.classList.toggle("filterActive");
+  navHb.classList.toggle("filterActive");
+});
 
 // TYPE WRITTER CLASS
 
 const AboutAnim = document.querySelector(".about-title");
-console.log('AboutAnim:', AboutAnim)
+console.log("AboutAnim:", AboutAnim);
 
 let typeWriter = new Typewriter(AboutAnim, {
   delay: 120,
   deleteSpeed: 40,
-  cursor: "."
-})
+  cursor: ".",
+});
 
 typeWriter
   .pauseFor(5500)
-  .typeString('Bonjour, Je suis Développeur...    ')
+  .typeString("Bonjour, Je suis Développeur..<br>")
   .pauseFor(150)
-  .typeString('<span style="color: #F1C40F">Javascript </span>')
+  .typeString('<span style="color: #F1C40F">JavaScript </span>')
   .pauseFor(400)
   .deleteChars(11)
   .typeString('<span style="color: #2980B9">React</span>')
@@ -56,8 +55,19 @@ typeWriter
   .deleteChars(6)
   .typeString('<span style="color: #8c444c">Web</span>')
   .pauseFor(500)
-  .deleteChars(23)
+  .deleteChars(18)
   .typeString('<span style="color: #345756">Axel Gourlin</span>')
   .pauseFor(1000)
   .start();
 
+// Work
+
+document.querySelector(".work-1").addEventListener("click", () => {
+  window.open("https://axelgourlin.github.io/restOdyssey/");
+});
+// document.querySelector('.work-2').addEventListener('click',() => {
+//   window.open('https://axelgourlin.github.io/');
+// })
+// document.querySelector('.work-3').addEventListener('click',() => {
+//   window.open('https://axelgourlin.github.io/');
+// })
